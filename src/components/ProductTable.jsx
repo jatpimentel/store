337181181b4b9481c726e8f56870cdf6f69f2fed
jatpimentel <jobaaron444@gmail.com>
2024-09-17 19:20:22 +0800

    Change Item Price

--- src/components/ProductTable.jsx
+++ src/components/ProductTable.jsx
@@ -22,26 +22,26 @@ const ProductTable = () => {
           </Table.Row>
           <Table.Row>
             <Table.Column>Billiard</Table.Column>
-            <Table.Column>$49.99</Table.Column>
+            <Table.Column>$99.99</Table.Column>
           </Table.Row>
           <Table.Row>
             <Table.Column>BasketBall</Table.Column>
-            <Table.Column>$49.99</Table.Column>
+            <Table.Column>$99.99</Table.Column>
           </Table.Row>
           <Table.Row>
             <Table.ColumnHeader colspan="2">Electronics</Table.ColumnHeader>
           </Table.Row>
           <Table.Row>
             <Table.Column>Ipad Touch</Table.Column>
-            <Table.Column>$49.99</Table.Column>
+            <Table.Column>$4=99.99</Table.Column>
           </Table.Row>
           <Table.Row>
             <Table.Column>Iphone 5</Table.Column>
-            <Table.Column>$49.99</Table.Column>
+            <Table.Column>$99.99</Table.Column>
           </Table.Row>
           <Table.Row>
             <Table.Column>Nexus 7</Table.Column>
-            <Table.Column>$49.99</Table.Column>
+            <Table.Column>$99.99</Table.Column>
           </Table.Row>
         </Table.TBody>
       </Table.TableContainer>
